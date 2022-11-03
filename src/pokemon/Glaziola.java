25d438f2226a0a09199c1b28a80d4d2b39aabe1d
LenHukiro <Niklas89@t-online.de>
2022-11-03 09:55:22 +0100

    Aufgabe 1

---
+++ src/pokemon/Glaziola.java
@@ -0,0 +1,7 @@
+package pokemon;
+
+public class Glaziola extends Pokemon implements IceType {
+    public Glaziola(String name, int hp, int attack) {
+        super("Glaziola", hp, attack);
+    }
+}
---
+++ src/pokemon/Pikachu.java
@@ -0,0 +1,7 @@
+package pokemon;
+
+public class Pikachu extends Pokemon implements ElectricType{
+    public Pikachu(String name, int hp, int attack) {
+        super("Pikachu", hp, attack);
+    }
+}
